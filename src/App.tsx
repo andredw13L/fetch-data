f199433af6610c67adb7c1c0aae8831deb5a734a
andredw13L <andreribeiro13l@gmail.com>
2024-06-25 14:46:13 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MyComponent from './components/Component'
+
+function App() {
+  return (
+    <>
+      <Component/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Component.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import Todo from './Todo'; 
+
+const Component: React.FC = () => {
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    useEffect(() => {
+        fetch('localhost:5000/todos', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((resp: Response) => {
+            if (!resp.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return resp.json();
+        })
+        .then((data: Todo[]) => {
+            setTodos(data);
+        })
+        .catch((err: Error) => console.log(err));
+    }, []);
+
+    return (
+        <div>
+            {/* Renderização dos todos */}
+            {todos.map(todo => (
+                <div key={todo.id}>
+                    <h3>{todo.title}</h3>
+                    <p>Completed: {todo.isDone ? 'Yes' : 'No'}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Component;
